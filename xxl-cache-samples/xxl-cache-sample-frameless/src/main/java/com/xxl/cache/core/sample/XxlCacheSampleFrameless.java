19079a144f0f2ddf376c5c10727a03e1dd48872b
xuxueli <931591021@qq.com>
2025-02-04 22:27:04 +0800

    refactor(xxl-cache-sample-frameless): 重构初始化和启动逻辑- 将初始化和启动 XxlCacheFactory 的代码提取到单独的方法 initAndStart 中 -简化了 main 方法的逻辑，使其更加清晰 - 提高了代码的可维护性和可测试性

--- xxl-cache-samples/xxl-cache-sample-frameless/src/main/java/com/xxl/cache/core/sample/XxlCacheSampleFrameless.java
+++ xxl-cache-samples/xxl-cache-sample-frameless/src/main/java/com/xxl/cache/core/sample/XxlCacheSampleFrameless.java
@@ -1,7 +1,6 @@
 package com.xxl.cache.core.sample;
 
 import com.xxl.cache.core.XxlCacheHelper;
-import com.xxl.cache.core.cache.CacheTypeEnum;
 import com.xxl.cache.core.factory.XxlCacheFactory;
 
 import java.io.IOException;
@@ -14,18 +13,8 @@ import java.util.concurrent.TimeUnit;
 public class XxlCacheSampleFrameless {
 
     public static void main(String[] args) throws InterruptedException {
-        // load config
-        Properties prop = loadClassPathProp("xxl-cache.properties");
-
-        // start
-        XxlCacheFactory xxlCacheFactory = new XxlCacheFactory();
-        xxlCacheFactory.setL1Provider(prop.getProperty("xxl.cache.l1.provider"));
-        xxlCacheFactory.setL2Provider(prop.getProperty("xxl.cache.l2.provider"));
-        xxlCacheFactory.setNodes(prop.getProperty("xxl.cache.l2.nodes"));
-        xxlCacheFactory.setUser(prop.getProperty("xxl.cache.l2.user"));
-        xxlCacheFactory.setPassword(prop.getProperty("xxl.cache.l2.password"));
-
-        xxlCacheFactory.start();
+        // init and start
+        XxlCacheFactory xxlCacheFactory = initAndStart();
 
         // test
         String category = "user";
@@ -41,6 +30,23 @@ public class XxlCacheSampleFrameless {
 
         // stop
         xxlCacheFactory.stop();
+    }
+
+    private static XxlCacheFactory initAndStart(){
+        // load config
+        Properties prop = loadClassPathProp("xxl-cache.properties");
+
+        // start
+        XxlCacheFactory xxlCacheFactory = new XxlCacheFactory();
+        xxlCacheFactory.setL1Provider(prop.getProperty("xxl.cache.l1.provider"));
+        xxlCacheFactory.setL2Provider(prop.getProperty("xxl.cache.l2.provider"));
+        xxlCacheFactory.setNodes(prop.getProperty("xxl.cache.l2.nodes"));
+        xxlCacheFactory.setUser(prop.getProperty("xxl.cache.l2.user"));
+        xxlCacheFactory.setPassword(prop.getProperty("xxl.cache.l2.password"));
+
+        xxlCacheFactory.start();
+
+        return xxlCacheFactory;
     }
 
     public static Properties loadClassPathProp(String propertyFileName) {
