1a586edeafa030773ea4f2450b1777ffdf73e4a3
qinz <qinz8@qq.com>
2025-03-13 22:45:11 +0800

    feat: 1.引入单元测试框架；2. 将原有的四个测试类改写成单元测试方式；3. 可能发现了一个 bug，具体见 todo

--- xxl-cache-core/src/test/java/com/xxl/cache/core/test/redis/RedisManagerTest.java
+++ xxl-cache-core/src/test/java/com/xxl/cache/core/test/redis/RedisManagerTest.java
@@ -3,28 +3,32 @@ package com.xxl.cache.core.test.redis;
 import com.xxl.cache.core.cache.Cache;
 import com.xxl.cache.core.cache.CacheValue;
 import com.xxl.cache.core.redis.RedisManager;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RedisManagerTest {
 
-    public static void main(String[] args) {
-
+    @Test
+    public void testRedisCRUD() {
         String nodes = "127.0.0.1:6379";
-        String user = null;
-        String password = null;
-
-        RedisManager l2CacheManager = new RedisManager(null,nodes, user, password);
+        RedisManager l2CacheManager = new RedisManager(null, nodes, null, null);
         l2CacheManager.start();
 
         Cache cache = l2CacheManager.getCache();
-
         String key = "user03";
 
+        // 测试删除
         cache.del(key);
-        System.out.println(cache.get(key));;
+        assertNull(cache.get(key));
 
+        // 测试写入和读取
         cache.set(key, new CacheValue("jack03"));
-        System.out.println(cache.get(key));;
+        assertEquals("jack03", cache.get(key).getValue());
 
+        // 测试重复删除
+        cache.del(key);
+        assertNull(cache.get(key));
     }
 
 }
